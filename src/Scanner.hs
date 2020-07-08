fed76874b337ca112edc95a29ee11b0b368ceec3
nini-faroux <nfarrel1@tcd.ie>
2020-07-08 19:36:54 +0100

    scan range of ports

--- src/Scanner.hs
+++ src/Scanner.hs
@@ -4,18 +4,21 @@
 module Scanner where
 
 import Import
+import GHC.IO.Exception (IOException(..))
+import Foreign.C.Error (Errno(..), eCONNREFUSED)
 
-import    GHC.IO.Exception (IOException(..))
-import    Foreign.C.Error (Errno(..), eCONNREFUSED)
+getOpenPorts :: IPAddress -> [PortNumber] -> IO [PortNumber]
+getOpenPorts address = foldM (\acc p -> checkPort address p >>= \s -> if s then return (p : acc) else return acc) []
 
 checkPort :: IPAddress -> PortNumber -> IO Bool
 checkPort address port = do
   let socketAddress = SockAddrInet port $ tupleToHostAddress address
   bracket (socket AF_INET Stream 6) close' $ \socket' -> do
-    response <- try $ connect socket' socketAddress
-    case response of
-      Right () -> return True
-      Left err ->
-        if (Errno <$> ioe_errno err) == Just eCONNREFUSED
-          then return False
-          else throwIO err
+       response <- timeout 1200000 $ try $ connect socket' socketAddress
+       case response of
+           Nothing -> return False
+           Just (Right ()) -> return True
+           Just (Left err) -> 
+             if (Errno <$> ioe_errno err) == Just eCONNREFUSED
+               then return False
+               else throwIO err
