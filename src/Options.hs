da56a0a0b46b8b9efb5e4f9b46674d066a9db792
nini-faroux <nfarrel1@tcd.ie>
2020-07-11 15:43:11 +0100

    cleaner port parsing, get rid of read functions

--- src/Options.hs
+++ src/Options.hs
@@ -4,7 +4,6 @@
 module Options where
 
 import Import
-import RIO.Partial (read)
 import qualified Data.ByteString.Char8 as BS8
 import qualified Data.Attoparsec.ByteString as P
 import qualified Data.Attoparsec.ByteString.Char8 as AC
@@ -19,44 +18,42 @@ portTarget :: Parser [PortNumber]
 portTarget = option portReader (short 'p' <> long "port" <> help "Check single port or range of ports")
 
 portReader :: ReadM [PortNumber]
-portReader = eitherReader (P.parseOnly parsePort . BS8.pack)
+portReader = eitherReader (P.parseOnly parsePortArgs . BS8.pack)
 
-parsePort :: P.Parser [PortNumber]
-parsePort = do
-  s <- AC.many1 AC.digit
+parsePortArgs :: P.Parser [PortNumber]
+parsePortArgs = do
+  s <- AC.decimal
   n <- P.peekWord8
   if isNothing n
-    then do
-      if checkPortInRange s 
-        then return [read s]
-        else fail "Invalid Input: Port must be in range 0-65535"
+    then case singlePortArg s of
+            Left err -> failPort err
+            Right ps -> return ps
     else do
       d <- AC.take 1
       if d /= "-"
-        then fail "Invalid Input: -p n or -p s-e"
+        then failPort InvalidPortFormat
         else do
-          e <- AC.many1 AC.digit
+          e <- AC.decimal
           n' <- P.peekWord8
           if isNothing n'
-            then if not (checkPortInRange e)
-                 then fail "Invalid Input: Port must be in range 0-65535"
-                 else do
-                   let check = checkInterval s e
-                   if not check 
-                     then fail "Invalid range parameters"
-                     else return [read s, read e]
-          else fail "Invalid Input: -p n or -p s-e"
+            then case multipleArgsPort s e of
+                    Left err -> failPort err
+                    Right ps -> return ps
+            else failPort InvalidPortFormat
 
-checkInterval :: String -> String -> Bool
-checkInterval s e
-  | (read s :: Int) > (read e :: Int) = False
-  | otherwise = True
+failPort :: PortError -> P.Parser [PortNumber]
+failPort pe = fail $ show pe
 
-checkPortInRange :: String -> Bool
-checkPortInRange s
-  | pn < 0 || pn > 65535 = False
-  | otherwise = True
-  where pn = read s :: Int
+singlePortArg :: Integer -> Either PortError [PortNumber]
+singlePortArg n
+  | n > 65535 = Left InvalidPortNumber
+  | otherwise = Right [fromIntegral n]
+
+multipleArgsPort :: Integer -> Integer -> Either PortError [PortNumber]
+multipleArgsPort s e
+  | s > e = Left InvalidPortRange
+  | e > 65535 = Left InvalidPortNumber
+  | otherwise = Right [fromIntegral s, fromIntegral e]
 
 verbose :: Parser Bool
 verbose = switch (short 'v' <> long "verbose" <> help "Verbose output")
--- src/Types.hs
+++ src/Types.hs
@@ -24,6 +24,15 @@ instance HasLogFunc App where
 instance HasProcessContext App where
   processContextL = lens appProcessContext (\x y -> x { appProcessContext = y })
 
+data PortError =
+    InvalidPortNumber
+  | InvalidPortFormat
+  | InvalidPortRange
+instance Show PortError where
+  show InvalidPortNumber = "Invalid Port Argument: Port Numbers must be between 0 and 65535"
+  show InvalidPortFormat = "Invalid Port Format, usage: -p <portnumber> or -p <startOfRange>-<endOfRange>"
+  show InvalidPortRange = "Invalid Port Range: Start of range must be less than or equal to end of range"
+
 data PortStatus =
     Open
   | Closed
