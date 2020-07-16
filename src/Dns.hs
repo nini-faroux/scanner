bf2526ec637a912fe9da07cd4b15eadf33d4b0ea
nini-faroux <nfarrel1@tcd.ie>
2020-07-16 15:10:08 +0100

    use rio import

--- src/Dns.hs
+++ src/Dns.hs
@@ -3,9 +3,9 @@
 module Dns where
 
 import Import
-import qualified Data.ByteString.Char8 as BS8
+import qualified RIO.ByteString as BS
 
-lookupIPv4 :: BS8.ByteString -> IO (Either DNSError [IPv4])
+lookupIPv4 :: BS.ByteString -> IO (Either DNSError [IPv4])
 lookupIPv4 hostName = do
     rs <- makeResolvSeed defaultResolvConf
     withResolver rs $ \resolver -> lookupA resolver hostName
